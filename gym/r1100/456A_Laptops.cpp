35a2cae75d404ba2f6474d5104d26e28f7f5620c
Sonok Mahapatra <sonokmahapatra@gmail.com>
2026-01-11 11:44:32 -0500

    Initalize Learning languages and finished laptops

--- gym/r1100/456A_Laptops.cpp
+++ gym/r1100/456A_Laptops.cpp
@@ -11,13 +11,21 @@ int main() {
     cin.tie(nullptr); // disable autoflush of cout
 
     int n; cin >> n;          // comment this if single test case
+    vector<pair<int,int>> vec; 
+
     for(int i = 0; i < n; i++) {
         int a,b;
         cin >> a >> b;
-        if(a + b != n + 1) {
-            cout << "Poor Alex" << "\n";
+        vec.emplace_back(a,b);
+    }
+    sort(vec.begin(), vec.end());
+
+    for(int i = 0; i < n - 1; i++) {
+        if(vec[i].second > vec[i+1].second) {
+            cout << "Happy Alex" << "\n";
+            return 0;
         }
     }
-    cout << "Happy Alex" << "\n";
+    cout << "Poor Alex" << "\n";
     return 0;
 }
