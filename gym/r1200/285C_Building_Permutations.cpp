ac1c8f48e682f120c0a3c5d9fd7ce00e2371e160
Sonok Mahapatra <sonokmahapatra@gmail.com>
2026-04-13 15:34:30 -0400

    Intresting nondecreasing + sorting

---
+++ gym/r1200/285C_Building_Permutations.cpp
@@ -0,0 +1,47 @@
+// how to compile g++ -std=gnu++17 -O2 main.cpp -o main && ./main
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <numeric>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<long long,long long>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+const ll INF = (ll)1e18;
+const int MOD = 1e9 + 7;
+
+void solve() {
+    // write solution here
+}
+
+int main() {
+    ios::sync_with_stdio(false);  // disconnect c++ streams and C stdio
+    cin.tie(nullptr); // disable autoflush of cout
+
+    priority_queue<int, vector<int>, greater<int>> pq; 
+    int n; cin >> n;          // comment this if single test case
+    for(int i = 0; i < n; i++) {
+        int x; cin >> x;
+        pq.push(x);
+    }
+    ll total = 0;
+    for(int i = 1; i <= n; i++) {
+        total += abs(pq.top() - i);
+        pq.pop();
+    }
+    cout << total;
+    return 0;
+}
